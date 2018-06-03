6826121b78e95dfdb4cbebca7460b99168cea4b1
Yihao Jiang <jiangyh91@gmail.com>
2018-06-04 01:27:34 +1000

    Add FlexLayout component

---
+++ src/components/flex-layout/index.ts
@@ -0,0 +1,11 @@
+import FlexLayout from './FlexLayout';
+
+export default FlexLayout;
+
+export {
+  justifyContentValue,
+  flexDirectionValue,
+  alignItemsValue,
+  alignContentValue,
+  flexWrapValue
+} from "./FlexLayout";
